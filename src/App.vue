b56de0608539782253ade69640c153826fcc22fa
Manuela <manu.cm43@gmail.com>
2018-10-18 10:55:13 -0500

    add node version update build files

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <div id="app">
+    <div class="welcome">
+      <div class="row middle-xs center-xs">
+        <div class="col-xs-12">
+          <img src="../src/assets/images/logo.svg" alt="codex logo">
+          <h1>This project will be amazing!</h1>
+        </div>
+      </div>
+    </div>
     <router-view></router-view>
   </div>
 </template>
@@ -9,3 +17,18 @@ export default {
   name: 'app'
 }
 </script>
+
+<style lang="scss">
+@import './styles/vars';
+@import './styles/functions';
+
+.welcome {
+  height: 100vh;
+  background-color: color(astronaut);
+  color: color(sunflower);
+
+  h1 {
+    font-weight: bold;
+  }
+}
+</style>
