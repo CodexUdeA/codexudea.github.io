f3fc2867befeb014b9d4bd9db0cf37313488cbb9
Andrés <andres.isazaa@udea.edu.co>
2018-10-18 20:01:10 -0500

    Creation of a button

---
+++ src/components/button.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="button">
+    <button>
+      {name}
+    </button>
+  </div> 
+</template>
+
+<script>
+export default {
+    name: "Click me!"
+};
+</script>
+
+<style lang="scss">
+  .button {
+  }
+</style>
+
+
+
