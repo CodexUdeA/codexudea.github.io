270a85bd95f2e2d8a4c6fb54167f7ff646ea6927
Andrés Pino Vallejo <andrespinovallejo@gmail.com>
2018-11-01 23:47:30 -0500

    Event card component structured following the mockup

--- src/components/event-card.vue
+++ src/components/event-card.vue
@@ -1,13 +1,39 @@
 <template>
-  <div class="event-card"></div>
+  <div class="event-card">
+    <div class="event-card__information">
+      <h2>Non tempor officia consequat ut consectetur.</h2>
+      <span>The date</span>
+      <span>The hour</span>
+      <span>The place</span>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'event-card'
-  }
+export default {
+  name: 'event-card'
+}
 </script>
 
 <style lang="scss">
-  .event-card {}
+  @import '../styles/vars';
+  @import '../styles/functions';
+  .event-card {
+    max-width: 300px;
+    height: 400px;
+    border-radius: 5px;
+    color: color(white);
+    background: linear-gradient(rgba(44, 53, 129, 0.7), rgba(44, 53, 129, 0.7)), url('http://placekitten.com/200/200');
+    &__information {
+      height: inherit;
+      padding: 50% 15px 15px 15px;
+      h2 {
+        font-weight: bold;
+      }
+      span {
+        display: block;
+        padding-top: 10px;
+      }
+    }
+  }
 </style>
