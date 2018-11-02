fdb32a49f0ba94c70870380e2ea26b575605e37e
gio96 <gio96gomez@gmail.com>
2018-11-02 17:08:49 -0500

    display

--- src/components/navbar-desktop.vue
+++ src/components/navbar-desktop.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="navbar-desktop">
-    <nav class="navbar-desktop-nav center-sm">
-      <ul class="row navbar-desktop-list col-sm-12">
-        <li class="navbar-desktop-item col-sm-2"><img src="../assets/images/logo.svg" alt="codex logo"></li>
-        <li class="navbar-desktop-item col-sm-1"><a href="#">CodeX</a></li>
-        <li class="navbar-desktop-item col-sm-1"><a href="#">Eventos</a></li>
-        <li class="navbar-desktop-item col-sm-1"><a href="#">Heroes</a></li>
-        <li class="navbar-desktop-item col-sm-1"><a href="#">Salón de la Fama</a></li>
-        <li class="navbar-desktop-item col-sm-1"><a href="#">Equipo</a></li>
-        <li class="navbar-desktop-item col-sm-1"><a href="#">Dar una Charla</a></li>
-        <li class="navbar-desktop-item col-sm-1"><a href="#">Patrocinadores</a></li>
-      </ul>
+    <nav class="navbar-desktop-nav">
+      <div class="row navbar-desktop-nav-list center-sm">
+        <li class="navbar-desktop-nav-item col-sm-2 middle-xs"><img src="../assets/images/logo.svg" alt="codex logo"></li>
+        <li class="navbar-desktop-nav-item col-sm-1 middle-xs"><a href="#">CodeX</a></li>
+        <li class="navbar-desktop-nav-item col-sm-1 middle-xs"><a href="#">Eventos</a></li>
+        <li class="navbar-desktop-nav-item col-sm-1 middle-xs"><a href="#">Heroes</a></li>
+        <li class="navbar-desktop-nav-item col-sm-1 middle-xs"><a href="#">Salón de la Fama</a></li>
+        <li class="navbar-desktop-nav-item col-sm-1 middle-xs"><a href="#">Equipo</a></li>
+        <li class="navbar-desktop-nav-item col-sm-1 middle-xs"><a href="#">Dar una Charla</a></li>
+        <li class="navbar-desktop-nav-item col-sm-1 middle-xs"><a href="#">Patrocinadores</a></li>
+      </div>
     </nav>
   </div>
 </template>
@@ -25,19 +25,25 @@ export default {
 @import '../styles/mixins';
 @import '../styles/vars';
 @import '../styles/functions';
+
+@media screen and(max-width: 767px){
+  .navbar-desktop{
+    display: none;
+  }
+}
+
 @include from('small'){
 .navbar-desktop{
   &-nav {
     width: 100%;
-    padding: 1em;
-    background-color:color('astronaut');
-    position: absolute;
+    padding: 20px;
+    background-color: color('astronaut');
 
-    ul {
+    &-list {
       margin: 0;
       list-style: none;
-      justify-content:center;
       align-items: center;
+      padding: 0 0 0 0;
     }
 
     &-item {
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import Navbar from '../components/navbar-desktop'
+
 export default {
   name: 'home-page'
 }
