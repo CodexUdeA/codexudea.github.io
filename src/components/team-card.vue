c29f5a10f17164e4866c20778ac3dd2e63aaa499
manucastrillonm <manu.cm43@gmail.com>
2018-11-06 11:06:00 -0500

    add team member info and color theme dynamically

--- src/components/team-card.vue
+++ src/components/team-card.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="team-card">
+  <div v-bind:class="['team-card', theme]">
     <div class="row center-xs">
       <div class="team-card-image-container">
-        <img class="team-card-image" src="http://placekitten.com/200/200" alt="team member picture">
+        <img class="team-card-image" v-bind:src="teamMember.picture" alt="team member picture">
       </div>
     </div>
-    <div class="row center-xs">
-      <h4 class="team-card-name">Lorem Ipsum</h4>
+    <div class="team-card-info">
+      <h4 class="team-card-name">{{teamMember.name}}</h4>
       <p class="team-card-bio">
-        Ullamco duis qui aute eiusmod aute elit officia exercitation commodo sint.
+        {{teamMember.bio}}
       </p>
       <div class="team-card-social">
-        <a href="#"><i class="fa fa-2x fa-github" aria-hidden="true"></i></a>
-        <a href="#"><i class="fa fa-2x fa-twitter" aria-hidden="true"></i></a>
+        <a v-bind:href="teamMember.github">
+          <i class="fa fa-2x fa-github" aria-hidden="true"></i>
+        </a>
+        <a v-bind:href="teamMember.twitter">
+          <i class="fa fa-2x fa-twitter" aria-hidden="true"></i>
+        </a>
       </div>
     </div>
   </div>
@@ -20,7 +24,14 @@
 
 <script>
 export default {
-  name: 'team-card'
+  name: 'team-card',
+  props: {
+    teamMember: {
+      type: Object,
+      required: true
+    },
+    theme: String
+  }
 }
 </script>
 
@@ -29,8 +40,14 @@ export default {
 @import '../styles/functions';
 
 .team-card {
-  background-color: color(chenin);
+
+  &.green {
+    background-color: color(chenin);
+  }
+
+  background-color: color(white);
   border-radius: 10px;
+  height: 341px;
   max-width: 235px;
   padding: 33px 19px;
   text-align: center;
