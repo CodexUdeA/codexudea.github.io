3d840c4efaaf555b56ba19daf8b8af0097882595
Andrés Pino Vallejo <andrespinovallejo@gmail.com>
2018-11-18 19:20:06 -0500

    Text section done (with a button problem)

--- src/components/text-section.vue
+++ src/components/text-section.vue
@@ -1,16 +1,74 @@
 <template>
-  <div class="text-section">
+  <div class="center-xs text-section row">
+    <div class="col-xs-12 title text-section__title">
+      <h1>{{title}}</h1>
+    </div>
+    <div class="col-xs-12 col-md-6 center-xs text-section__description">
+      <span>{{description}}</span>
+    </div>
+    <div class ="col-xs-12 text-section__button">
+      <Button
+        v-bind:label="buttonLabel"
+        v-bind:url="buttonUrl"
+        class="hero__button"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name:'text-section'
+import Button from './button/index'
+export default {
+  name: 'text-section',
+  components: {
+    Button
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: ''
+    },
+    description: {
+      type: String,
+      required: true,
+      default: ''
+    },
+    useButton: {
+      type: Boolean,
+      required: true,
+      default: false
+    },
+    buttonLabel: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    buttonUrl: {
+      type: String,
+      required: false,
+      default: ''
+    }
   }
+}
 </script>
 
-<style>
-  .text-section {
+<style lang="scss">
+  @import '../styles/vars';
+  @import '../styles/functions';
 
+  .text-section {
+    background: color(astronaut);
+    color: color(white);
+    &__title {
+      margin-top: 100px;
+    }
+    &__description {
+      margin-top: 80px;
+      font-size: font(font-16);
+    }
+    &__button {
+      margin: 100px 0;
+    }
   }
 </style>
