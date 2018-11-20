fae529d77d6933986085df3271453e37d3b8bbeb
Andrés Pino Vallejo <andrespinovallejo@gmail.com>
2018-11-20 08:14:00 -0500

    TextSection component added to home

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -8,10 +8,20 @@
       sub-headline="Lorem sint ullamco nisi irure ut ut magna et ex nulla."
       description="Aliqua commodo quis elit laborum. Proident eu laboris culpa est nostrud mollit. Cupidatat excepteur exercitation pariatur pariatur magna commodo dolore nostrud in. Exercitation consequat eiusmod cupidatat irure mollit ut sint cupidatat sit cupidatat."
     />
+    <text-section
+      title="¿Quienes Somos?"
+      description="Aliquip sint labore adipisicing irure. Labore reprehenderit sunt labore velit. Ullamco fugiat ad culpa anim officia eiusmod excepteur sint exercitation cillum. Consequat veniam laborum irure magna magna anim tempor aliquip reprehenderit quis voluptate laborum proident. Incididunt aliqua voluptate quis duis nostrud sunt quis aute nisi voluptate minim ut elit. Amet adipisicing minim in deserunt exercitation consectetur officia. Deserunt eu anim occaecat quis anim cillum est veniam ea sint id culpa ipsum."
+    />
     <next-events />
     <heroes-section />
     <hall-of-fame />
     <organizing-team />
+    <text-section
+      title="Quiero dar una charla"
+      description="Aliquip sint labore adipisicing irure. Labore reprehenderit sunt labore velit. Ullamco fugiat ad culpa anim officia eiusmod excepteur sint exercitation cillum. Consequat veniam laborum irure magna magna anim tempor aliquip reprehenderit quis voluptate laborum proident. Incididunt aliqua voluptate quis duis nostrud sunt quis aute nisi voluptate minim ut elit. Amet adipisicing minim in deserunt exercitation consectetur officia. Deserunt eu anim occaecat quis anim cillum est veniam ea sint id culpa ipsum."
+      useButton="true"
+      buttonLabel="Fill Form"
+    />
     <sponsors />
   </div>
 </template>
@@ -24,6 +34,7 @@ import NavbarDesktop from '../components/navbar-desktop'
 import NextEvents from '../components/next-events'
 import OrganizingTeam from '../components/organizing-team'
 import Sponsors from '../components/sponsors'
+import TextSection from '../components/text-section'
 
 export default {
   name: 'home-page',
@@ -34,7 +45,8 @@ export default {
     NavbarDesktop,
     NextEvents,
     OrganizingTeam,
-    Sponsors
+    Sponsors,
+    TextSection
   },
   data () {
     return {
