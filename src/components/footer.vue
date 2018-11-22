52a16a01f3a30060a326c87b43c51cf35da7d9fc
PilarArroyave <mdelpilar.arroyave@udea.edu.co>
2018-11-21 20:10:11 -0500

    Advance of footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="footer">
-    <img src="../assets/images/logo.svg" alt="codex logo">
-    <div class="contact">
-      <i class="fa fa-facebook-official" aria-hidden="true" />/codexUdeA
-      <i class="fa fa-at" aria-hidden="true" />info@codex.com
-      <div class="message">
-        Made with <i class="fa fa-heart" aria-hidden="true" />
-      </div>
+  <div class="footer row">
+    <div class="logo">
+      <img class="footer-image" src="../assets/images/logo.svg" alt="codex logo">
+    </div>
+    <div class="facebook-contact">
+      <i class="fa fa-facebook-official" aria-hidden="true" />
+      <label class= "facebook"> /codexUdeA </label>
+    </div>
+    <div class="email-contact">
+      <i class="fa fa-at" aria-hidden="true" />
+      <label class="email"> info@codex.com </label>
+    </div>
+    <div class="message">
+        <label class="message-txt"> Made with </label>
+        <i class="fa fa-heart" aria-hidden="true" />
     </div>
   </div>
 </template>
@@ -20,70 +27,149 @@ export default {
 <style lang="scss">
   @import '../styles/vars';
   @import '../styles/functions';
-  @font-face {
-    font-family:'Roboto';
-    src: url('../assets/fonts/Roboto-Regular.ttf') format("truetype");
-  }
+  @import '../styles/mixins';
 
   .footer {
     background-color: color(astronaut);
     color: white;
-    height: 65px;
+    height: 84px;
+    width: 1800px;
+    margin: 0px;
     padding: 0px;
-    border-top: 2px solid white;
-    border-left: none;
-    border-right: none;
-    border-bottom: none;
-    font-family: "Roboto";
-    font-weight: lighter;
+    font-weight: font-weight(light);
+    font-size: fonts(font-16);
+  }
+
+  .logo .footer-image {
+    width: 71px;
+    height: 27px;
+    margin: 28px 400px 29px 352px;
+    padding: 0px;
+    float: left;
+  }
+
+  .facebook-contact .fa-facebook-official {
+    width: 24px;
+    height: 24px;
+    margin: 34px 10px 26px 0px;
+    padding: 0px;
+    font-size: 24px;
+    float: left;
+  }
+
+  .facebook-contact .facebook {
+    width: 87px;
+    height: 19px;
+    margin: 37px 36px 28px 0px;
+    padding: 0px;
     font-size: 16px;
-
-    @media (max-width: 500px) {
-      height: 45px;
-      font-size: 8px;
-    }
+    float: left;
   }
 
-  .footer img {
-    margin: 20px 250px;
-    width: 60px;
-
-    @media (max-width: 500px) {
-      margin: 15px 2% 15px 10%;
-      width: 30px;
-    }
-  }
-
-  .footer div {
-    margin: 20px 250px 20px 50px;
+  .email-contact .fa-at {
+    width: 24px;
+    height: 24px;
+    margin: 34px 6px 26px 0px;
     padding: 0px;
-    float: right;
-
-    @media (max-width: 500px) {
-      margin: 15px 10% 15px 2%;
-    }
+    font-size: 24px;
+    float: left;
   }
 
-  .footer i {
-    margin: 0px 5px 0px 0px;
-    font-size: 20px;
-
-    @media (max-width: 500px){
-      margin: 0px 3px 0px 2px;
-      font-size: 10px;
-    }
+  .email-contact .email {
+    width: 119px;
+    height: 19px;
+    margin: 37px 66px 28px 0px;
+    padding: 0px;
+    font-size: 16px;
+    float: left;
   }
 
-  .contact .fa-heart {
-    margin: 0px 3px;
+  .message .message-txt {
+    width: 74px;
+    height: 19px;
+    margin: 37px 9px 28px 0px;
+    padding: 0px;
+    font-size: 16px;
+    float: left;
+  }
+
+  .message .fa-heart {
     color: #DB5757;
+    width: 24px;
+    height: 20.73px;
+    margin: 37px 326px 26.27px 0px;
+    font-size: 24px;
+    padding: 0px;
+    float: left;
   }
 
-  .contact .message {
-    margin: 0px 15px 0px 10px;
+  @media (max-width: 1200px) {
+    .footer {
+      height: 84px;
+      width: 100%;
+    }
 
-    @media (max-width: 500px){
-      margin: 0px 3px 0px 5px;
+    .logo .footer-image {
+      width: 3.94%;
+      height: 27px;
+      margin: 28px 352px;
+      padding: 0px;
+      float: left;
+    }
+
+    .facebook-contact .fa-facebook-official {
+      width: 24px;
+      height: 24px;
+      margin: 34px 10px 26px 0px;
+      padding: 0px;
+      font-size: 24px;
+      float: left;
+    }
+
+    .facebook-contact .facebook {
+      width: 87px;
+      height: 19px;
+      margin: 37px 36px 28px 0px;
+      padding: 0px;
+      font-size: 16px;
+      float: left;
+    }
+
+    .email-contact .fa-at {
+      width: 24px;
+      height: 24px;
+      margin: 34px 6px 26px 0px;
+      padding: 0px;
+      font-size: 24px;
+      float: left;
+    }
+
+    .email-contact .email {
+      width: 119px;
+      height: 19px;
+      margin: 37px 66px 28px 0px;
+      padding: 0px;
+      font-size: 16px;
+      float: left;
+    }
+
+    .message .message-txt {
+      width: 75px;
+      height: 19px;
+      margin: 37px 9px 28px 0px;
+      padding: 0px;
+      font-size: 16px;
+      float: left;
+    }
+
+    .message .fa-heart {
+      color: #DB5757;
+      width: 24px;
+      height: 20.73px;
+      margin: 37px 326px 26.27px 0px;
+      font-size: 24px;
+      padding: 0px;
+      float: left;
     }
   }
 </style>
