58934aeeae03e53d707212b933d7fcf376a732f7
Sebastian Vargas L <svargaslondono@gmail.com>
2019-10-30 00:18:57 -0500

    [#123] Footer links

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -8,16 +8,24 @@
         <div class="footer-information col-xs-9 col-sm-8 col-md-7 col-lg-6">
           <div class="row middle-xs">
             <div class="footer-social">
-              <i class="fa fa-facebook-official" aria-hidden="true" />
-              <span class= "footer-social__text">/codexUdeA</span>
+              <a target="blank" aria-label="CodexUdeA Facebook"
+                href="https://www.facebook.com/groups/CodexUdeA/">
+                <i class="fa fa-facebook-official" aria-hidden="true" />
+                <span class= "footer-social__text">/groups/CodexUdeA</span>
+              </a>
             </div>
             <div class="footer-social">
-              <i class="fa fa-at" aria-hidden="true" />
-              <span class="footer-social__text">codex@udea.edu.co</span>
+              <a href="mailto:codex@udea.edu.co">
+                <i class="fa fa-at" aria-hidden="true" />
+                <span class="footer-social__text">codex@udea.edu.co</span>
+              </a>
             </div>
             <div class="footer-social">
-              <span> Made with </span>
-              <i class="fa fa-heart" aria-hidden="true" />
+              <a target="blank" aria-label="CodexUdeA GitHub repository"
+                href="https://github.com/CodexUdeA/codexudea.github.io">
+                <span> Made with </span>
+                <i class="fa fa-heart" aria-hidden="true" />
+              </a>
             </div>
           </div>
         </div>
@@ -47,6 +55,15 @@ export default {
 
   .footer-social {
     margin-right: 20px;
+
+    a {
+      color: unset;
+      text-decoration: none;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
   }
 
   .footer-logo .footer-image {
