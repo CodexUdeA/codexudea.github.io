09a5805686d3c72bda19a4454333086ce8df7e62
manuCastrillonM <manu.cm43@gmail.com>
2021-09-20 20:38:55 -0500

    fix image url on sponsors section

--- src/components/Sponsors.vue
+++ src/components/Sponsors.vue
@@ -4,51 +4,62 @@
       <h1>Patrocinadores</h1>
     </div>
     <div class="row center-xs middle-xs">
-      <div class="col-xs-12 col-sm-6 col-md-3" v-for="item in sponsors" :key="item.name">
-        <card v-bind:logo="item.logo" v-bind:sponsor_name="item.name" v-bind:sponsor_url="item.url"/>
+      <div
+        class="col-xs-12 col-sm-6 col-md-3"
+        v-for="item in sponsors"
+        :key="item.name"
+      >
+        <card
+          v-bind:logo="item.logo"
+          v-bind:sponsor_name="item.name"
+          v-bind:sponsor_url="item.url"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import sponsorCard from '@/components/SponsorsCard'
+import sponsorCard from "@/components/SponsorsCard";
 
 export default {
-  name: 'sponsors',
+  name: "sponsors",
   data: function () {
-    return { sponsors: [{
-      name: 'Huge inc',
-      logo: '/src/assets/images/sponsors/logo_huge.jpg',
-      url: 'http://www.hugeinc.com'
-    },
-    {
-      name: 'GitHub Campus Experts',
-      logo: '/src/assets/images/sponsors/logo_gce.png',
-      url: 'http://www.github.com'
-    },
-    {
-      name: 'Laboratorio Integrado de Sistemas',
-      logo: '/src/assets/images/sponsors/logo_lis.jpeg',
-      url: 'http://lis.udea.edu.co'
-    },
-    {
-      name: 'Departamento de Ingenieria de Sistemas',
-      logo: '/src/assets/images/sponsors/logo_ids.jpg',
-      url: 'http://sistemas.udea.edu.co'
-    }]
-    }
+    return {
+      sponsors: [
+        {
+          name: "Huge inc",
+          logo: "logo_huge.jpg",
+          url: "http://www.hugeinc.com",
+        },
+        {
+          name: "GitHub Campus Experts",
+          logo: "logo_gce.png",
+          url: "http://www.github.com",
+        },
+        {
+          name: "Laboratorio Integrado de Sistemas",
+          logo: "logo_lis.jpeg",
+          url: "http://lis.udea.edu.co",
+        },
+        {
+          name: "Departamento de Ingenieria de Sistemas",
+          logo: "logo_ids.jpg",
+          url: "http://sistemas.udea.edu.co",
+        },
+      ],
+    };
   },
   components: {
-    card: sponsorCard
-  }
-}
+    card: sponsorCard,
+  },
+};
 </script>
 
 <style lang="scss">
-@import '../styles/vars';
-@import '../styles/mixins';
-@import '../styles/functions';
+@import "../styles/vars";
+@import "../styles/mixins";
+@import "../styles/functions";
 
 .sponsors {
   margin: 0 auto;
--- src/components/SponsorsCard.vue
+++ src/components/SponsorsCard.vue
@@ -1,35 +1,39 @@
 <template>
   <div class="sponsor-card">
-    <a v-bind:href="sponsor_url">
-      <img v-bind:src="logo" v-bind:alt="sponsor_name" class="sponsor-card__image">
+    <a :href="sponsor_url">
+      <img
+        :src="require(`../assets/images/sponsors/${logo}`)"
+        :alt="sponsor_name"
+        class="sponsor-card__image"
+      />
     </a>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'sponsor-card',
+  name: "sponsor-card",
   props: {
     logo: {
       type: String,
-      required: true
+      required: true,
     },
     sponsor_name: {
       type: String,
-      required: true
+      required: true,
     },
     sponsor_url: {
       type: String,
-      required: true
-    }
-  }
-}
+      required: true,
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-@import '../styles/vars';
-@import '../styles/mixins';
-@import '../styles/functions';
+@import "../styles/vars";
+@import "../styles/mixins";
+@import "../styles/functions";
 
 .sponsor-card {
   margin-bottom: 30px;
