a4aef86a892683a7abdcd7b6ee298fda305933b1
manuCastrillonM <manu.cm43@gmail.com>
2021-09-21 22:22:29 -0500

    refactor hero component

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,37 +1,26 @@
 <template>
   <div class="hero">
-    <div class="hero__image">
-      <img
-        class="hero__image-img"
-        :src="require('/src/assets/images/hero/bg-hero.jpg')"
-      >
-    </div>
-    <div class="hero__body">
-      <h1 class="hero__headline">
-        Codex UdeA
+    <div class="hero_container">
+      <h1 class="hero_title">
+        {{ title }}
       </h1>
-      <h2 class="hero__sub-headline">
-        Somos estudiantes a los que nos apasiona el desarrollo de software y nos
-        reunimos para aprender juntos.
+      <h2 class="hero_description">
+        {{ description }}
       </h2>
-      <Button
-        label="Dar una charla"
-        url="https://docs.google.com/forms/d/e/1FAIpQLSfyeovAO7oIL4Zj3TdHXKJMKBzTo0d9EVxnpU4PgaEtEb4SxA/viewform"
-        target="_blank"
-        type="anchor"
-        class="hero__button"
-      />
     </div>
+    <span class="hero_bg" />
   </div>
 </template>
 
 <script>
-import button from "./Button/";
-
 export default {
   name: "Hero",
-  components: {
-    Button: button,
+  data() {
+    return {
+      title: "Codex UdeA",
+      description:
+        "Somos una comunidad de estudiantes de la Universidad de Antioquia apasionados por el desarrollo de software",
+    };
   },
 };
 </script>
@@ -39,108 +28,71 @@ export default {
 <style lang="scss">
 @import "../styles/vars";
 @import "../styles/mixins";
-@import "../styles/functions";
 
 .hero {
-  align-items: center;
-  background-color: #000022;
-  color: color(white);
-  display: flex;
+  background: linear-gradient(
+      to bottom,
+      rgba(70, 73, 163, 0.85),
+      rgba(70, 73, 163, 0.9)
+    ),
+    url("../assets/backgrounds/hero-img.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  color: #fff;
+  font-family: "Montserrat", sans-serif;
   height: 100vh;
-  justify-content: center;
   position: relative;
+  min-height: 600px;
 
-  &,
-  &__image {
-    min-height: 600px;
+  @include from(sm) {
+    height: 80vh;
   }
 
-  &__image {
+  @include from(md) {
+    min-height: 800px;
+  }
+
+  &_bg {
+    background-image: url("../assets/backgrounds/hero-path.svg");
+    background-position: bottom;
+    background-repeat: no-repeat;
+    bottom: 0;
+    height: 100%;
     position: absolute;
-    left: 0;
     top: 0;
     width: 100%;
-    height: 100%;
-    overflow: hidden;
-
-    &::after {
-      content: "";
-      background-color: rgba(#0d155e, 0.37);
-      border-radius: 100%;
-      display: block;
-      height: 120vw;
-      left: 50%;
-      position: absolute;
-      top: 50%;
-      transform: translate(-50%, -50%);
-      width: 120vw;
-
-      @include from(small) {
-        width: 120vh;
-        height: 120vh;
-      }
-
-      @include from(medium) {
-        width: 140vh;
-        height: 140vh;
-      }
-    }
   }
 
-  &__image-img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    opacity: 0.22;
-  }
-
-  &__body {
+  &_container {
     align-items: center;
     display: flex;
-    flex-basis: 100%;
     flex-direction: column;
-    padding: 0 15px;
-    position: relative;
+    justify-content: center;
+    height: 100%;
+    margin: 0 auto;
+    max-width: 90%;
     text-align: center;
-    z-index: 0;
 
-    @include from(medium) {
-      flex-basis: 70%;
-      padding: 0;
+    @include from(sm) {
+      max-width: 80%;
+    }
+
+    @include from(lg) {
+      max-width: 70%;
     }
   }
 
-  &__headline,
-  &__sub-headline {
-    font-size: 30px;
-    margin: 0;
+  &_title {
+    font-weight: 800;
+    font-size: 75px;
 
-    @include from(small) {
-      font-size: 36px;
+    @include from(sm) {
+      mfont-size: 85px;
     }
 
-    @include from(medium) {
-      font-size: 52px;
-    }
-  }
-
-  &__headline {
-    font-weight: font-weight(bold);
-  }
-
-  &__sub-headline {
-    font-weight: font-weight(medium);
-  }
-
-  &__button {
-    margin-top: 1.5rem;
-
-    @include from(small) {
-      margin-top: 30px;
-    }
-
-    @include from(medium) {
-      margin-top: 100px;
+    @include from(lg) {
+      font-size: 95px;
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,52 +1,16 @@
 <template>
   <div class="home-page">
-    <navbar-desktop />
     <hero />
-    <text-section
-      title="¿Quiénes somos?"
-      :use-button="false"
-      description="Somos una comunidad de desarrollo de software formada por estudiantes de la Universidad de Antioquia, nuestro objetivo es compartir conocimiento sobre desarrollo web y mejorar nuestras habilidades de comunicación."
-    />
-    <next-events />
-    <heroes-section />
-    <hall-of-fame />
-    <organizing-team />
-    <text-section
-      id="speaker"
-      title="Quiero dar una charla"
-      description="Nos encantaría tenerte como invitado especial, si quieres compartir tu conocimiento con nosotros, solo debes completar el formulario y nos pondremos en contacto contigo."
-      :use-button="true"
-      button-label="Dar una charla"
-      button-url="https://docs.google.com/forms/d/e/1FAIpQLSfyeovAO7oIL4Zj3TdHXKJMKBzTo0d9EVxnpU4PgaEtEb4SxA/viewform"
-    />
-    <sponsors />
-    <bottom />
   </div>
 </template>
 
 <script>
-import Bottom from "@/components/Footer";
-import HallOfFame from "@/components/HallOfFame";
 import Hero from "@/components/Hero";
-import HeroesSection from "@/components/HeroesSection";
-import NavbarDesktop from "@/components/NavbarDesktop";
-import NextEvents from "@/components/NextEvents";
-import OrganizingTeam from "@/components/OrganizingTeam";
-import Sponsors from "@/components/Sponsors";
-import TextSection from "@/components/TextSection";
 
 export default {
   name: "HomePage",
   components: {
-    Bottom,
-    HallOfFame,
     Hero,
-    HeroesSection,
-    NavbarDesktop,
-    NextEvents,
-    OrganizingTeam,
-    Sponsors,
-    TextSection,
   },
 };
 </script>
