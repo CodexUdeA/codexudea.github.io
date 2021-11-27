19c2944d39eed6db65b78e27110d617d5e93ec0e
Manu Castrillon <manu.cm43@gmail.com>
2021-11-26 22:33:01 -0500

    update team description

--- src/assets/data/team.js
+++ src/assets/data/team.js
@@ -26,44 +26,40 @@ module.exports = [
   {
     name: "Martin Quintero",
     description:
-      "Sunt laboris ea velit minim laborum ex eu quis cillum laboris officia ex eiusmod Mollit consectetur non fugiat aliqua incididunt ad cillum aute est commodo occaecat esse irure laboris.",
+      "Soy cofundador y CTO de guane Enterprises. Me encanta el diseño de sistemas, el liderazgo de equipos y la construcción de conocimiento desde las comunidades.",
     picture: "https://avatars.githubusercontent.com/u/10585976?v=4",
     social: [
       {
         name: "linkedin",
-        url: "https://linkedin.com/",
+        url: "https://linkedin.com/in/martineliasq",
       },
       {
         name: "instagram",
-        url: "https://www.instagram.com/",
+        url: "https://www.instagram.com/martineliasq",
       },
       {
         name: "github",
-        url: "https://www.github.com",
+        url: "https://www.github.com/martineliasq",
+      },
+      {
+        name: "twitter",
+        url: "https://www.twitter.com/YoQuinteroO",
       },
     ],
   },
   {
     name: "Sebas Pelaez",
     description:
-      "Sunt laboris ea velit minim laborum ex eu quis cillum laboris officia ex eiusmod Mollit consectetur non fugiat aliqua incididunt ad cillum aute est commodo occaecat esse irure laboris.",
+      "Ingenierio de sistemas de la UdeA, Data Scientist en La Haus apasionado por el Machine Learning, Deep Learning la programación y el Crossfit.",
     picture: "https://avatars.githubusercontent.com/u/15000005?v=4",
     social: [
       {
         name: "twitter",
-        url: "https://www.twitter.com",
-      },
-      {
-        name: "linkedin",
-        url: "https://linkedin.com/",
+        url: "https://www.twitter.com/SebasPVilla",
       },
       {
         name: "instagram",
-        url: "https://www.instagram.com/",
-      },
-      {
-        name: "github",
-        url: "https://www.github.com",
+        url: "https://www.instagram.com/sebaspelaezv",
       },
     ],
   },
@@ -84,42 +80,45 @@ module.exports = [
     ],
   },
   {
-    name: "Angie Castañeda",
-    description:
-      "Sunt laboris ea velit minim laborum ex eu quis cillum laboris officia ex eiusmod Mollit consectetur non fugiat aliqua incididunt ad cillum aute est commodo occaecat esse irure laboris.",
-    picture: "https://avatars.githubusercontent.com/u/42279901?v=4",
-    social: [
-      {
-        name: "twitter",
-        url: "https://www.twitter.com",
-      },
-      {
-        name: "instagram",
-        url: "https://www.instagram.com/",
-      },
-      {
-        name: "github",
-        url: "https://www.github.com",
-      },
-    ],
-  },
-  {
     name: "Pilar Arroyave",
     description:
-      "Sunt laboris ea velit minim laborum ex eu quis cillum laboris officia ex eiusmod Mollit consectetur non fugiat aliqua incididunt ad cillum aute est commodo occaecat esse irure laboris.",
+      "Ingeniera de Sistemas de la UdeA, disfruto los retos y aprender cosas nuevas. Me gusta mucho la naturaleza y compartir con personas que me llenen el alma.",
     picture: "https://avatars.githubusercontent.com/u/30705652?v=4",
     social: [
       {
         name: "twitter",
-        url: "https://www.twitter.com",
+        url: "https://www.twitter.com/PiliArroyave",
       },
       {
         name: "instagram",
-        url: "https://www.instagram.com/",
+        url: "https://www.instagram.com/pili_arroyave",
       },
       {
         name: "github",
-        url: "https://www.github.com",
+        url: "https://www.github.com/PilarArroyave",
+      },
+      {
+        name: "linkedin",
+        url: "https://linkedin.com/in/pilar-arroyave",
+      },
+    ],
+  },
+  {
+    name: "Valentina Muñoz R",
+    description: "Estudiante de Ingeniería de Sistemas de la UdeA, curiosa por la tecnología, me encanta el cine y amante a la lectura.",
+    picture: "https://avatars.githubusercontent.com/u/79924429?v=4",
+    social: [
+      {
+        name: "twitter",
+        url: "https://www.twitter.com/VMR1010",
+      },
+      {
+        name: "instagram",
+        url: "https://www.instagram.com/valentina_mr10",
+      },
+      {
+        name: "github",
+        url: "https://www.github.com/ValenMR",
       },
     ],
   },
